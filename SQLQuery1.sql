e18669fa5de719793ace4ae10506657b55eedcfb
Bhavanas08 <90446584+Bhavanas08@users.noreply.github.com>
2023-01-18 18:51:11 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿select * from Position
+
+create proc InsertPositionDetails(@Positioncode varchar(50),
+                                  @Description varchar(500),
+                                  @year int,
+                                  @budgetedstrength int,
+                                  @currentstrength int)
+
+as
+begin
+    Insert into Positontable (cPositioncode, vDescription, siYear, ibudgetedstrength,icurrentstrength) values (@Positioncode, @Description, @year, @budgetedstrength,@currentstrength)
+end
