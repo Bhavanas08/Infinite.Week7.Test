52f5bd29aaad051e6d34d53bf79a2d8f36ec2860
Bhavanas08 <90446584+Bhavanas08@users.noreply.github.com>
2023-01-18 20:05:52 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿select * from Position
+
+alter proc InsertPositionDetails(@Positioncode char(4),
+                                  @Description varchar(35),
+                                  @year smallint,
+                                  @budgetedstrength int,
+                                  @currentstrength int)
+
+as
+begin
+    Insert  Position(cPositioncode, vDescription, siYear, ibudgetedstrength,icurrentstrength) values (@Positioncode, @Description, @year, @budgetedstrength,@currentstrength)
+end
+
+exec InsertPositionDetails '1234','abc','2024',32,24
